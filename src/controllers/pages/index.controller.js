ae076946963e06c37264a6913ffb4e6622ab7fd3
Melissa Pereira <melissa.pereira@kinandcarta.com>
2022-05-22 14:56:54 +0100

    Using ejs

--- src/controllers/pages/index.controller.js
+++ src/controllers/pages/index.controller.js
@@ -4,12 +4,21 @@ const { getContactList } = require ('../../services/contact.service')
 const getHome = (req, res) => {
     // res.send('Hello from your friendly Express Server')
     // Note: send() is only to send HTML response ('Content-Type is set to 'text/html')
-    res.sendFile( path.join( process.cwd(), 'public/index.html' ))
+    // res.sendFile( path.join( process.cwd(), 'public/index.html' ))
+    res.render('index', {
+        title: 'Stadium',
+        activeLink: req.url
+    })
 }
 
 const getAbout = (req, res) => {
     // res.send('We will build out the "about" page here')
-    res.sendFile( path.join( process.cwd(), 'public/about.html' ))
+    // res.sendFile( path.join( process.cwd(), 'public/about.html' ))
+    res.render( 'about', {
+        title: 'About | Stadium',
+        activeLink: req.url
+
+    })
 }
 
 const getContact = (req, res) => {
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@
 
 require( 'dotenv').config();
 require( './data/connect');
-
+const path = require( 'path' );
 const express = require( 'express' );
 const indexPageRouter = require( './routes/pages/index.route')
-const postsApiRouter = require( './routes/api/posts.route')
+const postsApiRouter = require( './routes/api/posts.route');
+const req = require('express/lib/request');
 
 //Express Application Object
 const app = express();
+
+app.set( 'views', path.join(process.cwd(), 'views') );
+app.set( 'view engine', 'ejs' );
 
 app.use( express.json()); // returns a middleware, called on every request, checks if the request has a body and if so, it makes it available on req.body
 app.use( indexPageRouter );
