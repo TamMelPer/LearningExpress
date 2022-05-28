2a668573f9edc28c57167f1708d52c07e91e25b9
Melissa Pereira <melissa.pereira@kinandcarta.com>
2022-05-28 12:57:00 +0100

    Add validation for email and password

--- src/models/User.js
+++ src/models/User.js
@@ -20,6 +20,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+const emailPattern = /^[A-Za-z\._]+@example\.com$/;
+const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+userSchema.path( 'email').validate(value => {
+    return emailPattern.test(value)
+}, 'Email `{VALUE}` is not a valid email address from example.com domain')
+
+userSchema.path( 'password').validate(value => {
+    return passwordPattern.test(value)
+}, 'Password must contain at least 8 characters, one uppercase, one lowercase, a digit and a special character.')
+
+
 const User = mongoose.model( 'User', userSchema );
 
 module.exports = User;
